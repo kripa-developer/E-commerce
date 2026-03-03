68e9124194370a8d2a7acbc030cdb8e05b029d52
Kripa Krishnan <kripakrishnan201@gmail.com>
2026-03-03 12:31:53 +0530

    Convert repo to real Angular workspace with login component

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from './components/login/login.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [LoginComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {}
